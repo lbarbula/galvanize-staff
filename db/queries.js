7040b23de9e0f73b48f3fab4bba03ff7a5badd07
Lucas Barbula <lucas.barbula@gmail.com>
2016-07-11 15:33:44 -0600

    working add staff feature

--- db/queries.js
+++ db/queries.js
@@ -3,5 +3,8 @@ var knex = require('./knex')
 module.exports = {
     listStaff: () => {
       return knex('staff')
-  }
+  },
+    addStaff: (model) => {
+      return knex('staff').insert(model)
+    }
 }
--- routes/staff.js
+++ routes/staff.js
@@ -13,4 +13,19 @@ router.get('/', function(req, res, next) {
     })
 });
 
+router.get('/new', function(req, res, next){
+  res.render('addStaff')
+})
+
+router.post('/',  function(req, res, next){
+  var staff = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name
+  }
+  query.addStaff(staff)
+    .then(function(){
+      res.redirect('/staff')
+    })
+})
+
 module.exports = router;
